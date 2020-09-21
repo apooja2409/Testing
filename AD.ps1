cd27092984b1c1fc9ac026899db028a3128b98a2
apooja2409 <71640431+apooja2409@users.noreply.github.com>
2020-09-21 21:43:55 +0530

    Update AD.ps1

--- AD.ps1
+++ AD.ps1
@@ -1,5 +1,5 @@
 $user = "AM\apooja"
-$secpasswd = ConvertTo-SecureString "1asdfghjkl;'" -AsPlainText -Force
+$secpasswd = ConvertTo-SecureString "**********" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
 (Get-ADGroup "IS-MSP_WindowsAdmin-PRD" -Server "am.corp.amgen.com" -Credential $cred).DistinguishedName
 
