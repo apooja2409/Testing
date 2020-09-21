40b46f9e7bc40cf8e746e5acc51ae6e583a509f1
Admin - Pooja <apooja-adm@amgen.com>
2020-09-21 21:07:02 +0530

    First Commit

---
+++ AD.ps1
@@ -0,0 +1,6 @@
+﻿import-module activedirectory
+$user = "AM\apooja"
+$secpasswd = ConvertTo-SecureString "1asdfghjkl;'" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
+(Get-ADGroup "IS-MSP_WindowsAdmin-PRD" -Server "am.corp.amgen.com" -Credential $cred).DistinguishedName
+
