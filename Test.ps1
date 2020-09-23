ee16fef753915908bfdfa435afcc0923cf47349b
apooja2409 <71640431+apooja2409@users.noreply.github.com>
2020-09-23 22:06:37 +0530

    Update and rename AD.ps1 to Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,5 +1,5 @@
-$user = "AM\apooja"
+$user = "yourUserID"
 $secpasswd = ConvertTo-SecureString "*************" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
-(Get-ADGroup "IS-MSP_WindowsAdmin-PRD" -Server "am.corp.amgen.com" -Credential $cred).DistinguishedName
+(Get-ADGroup "ADgrpName" -Server "hostname" -Credential $cred).DistinguishedName
 
