a2938190e1f3399c7474a427630ac66f4f114dd7
apooja2409 <71640431+apooja2409@users.noreply.github.com>
2020-09-25 14:41:29 +0530

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,4 +1,14 @@
-Param($nam)
-
-Write-Host $nam
+Param (
+   $nam = $Null,
+   [switch]$Recurse,
+   [switch]$Help
+   )
+   
+If($nam -eq $Null)
+{
+write-host "No Value"
+}
+Else{
+    write-host $nam
+    }
 
